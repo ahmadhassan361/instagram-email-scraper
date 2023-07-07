a65aa312020f8db77fa7eadadb67b3bf4db0e883
AHMAD HASSAN <techmastersx@gmail.com>
2023-07-07 11:50:24 +0500

    commit

---
+++ main.py
@@ -0,0 +1,113 @@
+import os
+import csv
+import time
+import requests
+BASE_URL = "https://instagram-scraper-2022.p.rapidapi.com/ig"
+headers = {
+	"X-RapidAPI-Key": "",
+	"X-RapidAPI-Host": "instagram-scraper-2022.p.rapidapi.com"
+}
+
+followers_list = []
+
+def append_row_to_csv(file_path, row_data):
+    file_exists = os.path.isfile(file_path)
+    mode = "a" if file_exists else "w"
+    if mode == "w":
+        header = [
+            "UserId",
+            "Username",
+            "FullName",
+            "Followers Count",
+            "Following Count",
+            "Post Count",
+            "Public Email",
+            "Country Code",
+            "Public Phone",
+            "Whatsapp Number",
+            "Profile Pic URL",
+            "Profile Visibility",
+        ]
+        with open(file_path, mode, newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+    with open(file_path, "a", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(row_data)
+
+def getUserId(username):
+    param = {
+        "user":username
+    }
+    res = requests.get(
+        f"{BASE_URL}/info_username/",headers=headers,params=param
+    )
+
+    data = res.json()
+    if data.get('status',None) == "ok":
+        return data.get('user').get('pk')
+
+def getUser(username):
+    param = {
+        "user":username
+    }
+    res = requests.get(
+        f"{BASE_URL}/info_username/",headers=headers,params=param
+    )
+
+    data = res.json()
+    if data.get('status',None) == "ok":
+        return data.get('user')
+    
+def getFollowers(userId,max_id = None):
+    param = {
+        'id_user':userId
+    }
+    if max_id is not None:
+        param['next_max_id'] = max_id
+    request = requests.get(
+        f"{BASE_URL}/followers/",headers=headers,params=param
+    )
+    res = request.json()
+    if res.get('status',None) == "ok" and res.get('next_max_id',None) is not None:
+
+        users = res.get('users',[])
+        public_users = [item for item in users if not item["is_private"]]
+        followers_list.extend(public_users)
+        print("fetched and again call")
+        getFollowers(userId=userId,max_id=res.get('next_max_id'))
+    elif res.get('status',None) == "ok":
+        users = res.get('users',[])
+        public_users = [item for item in users if not item["is_private"]]
+        followers_list.extend(public_users)
+        print("finished")
+
+
+page = input("Enter Instagram Page Name: ")
+file_path = f"output/{page}.csv"
+user = getUserId(page)
+print(user)
+getFollowers(userId=user)
+
+for i in followers_list:
+    usr = getUser(i['username'])
+    print("run")
+    if usr.get('public_email',None) is not None and usr.get('public_email',None) != "":
+        temp = [
+                usr.get("pk", ""),
+                usr.get("username", ""),
+                usr.get("full_name", ""),
+                usr.get("follower_count", ""),
+                usr.get("following_count", ""),
+                usr.get("media_count", ""),
+                usr.get("public_email",""),
+                usr.get("public_phone_country_code", ""),
+                usr.get("public_phone_number", ""),
+                usr.get("whatsapp_number", ""),
+                usr.get("profile_pic_url", ""),
+                "No",
+            ]
+        append_row_to_csv(file_path=file_path,row_data=temp)
+
+
+
